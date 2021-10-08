995f03a85f0d490b9868a5a156f393a4307b5266
dragonyu1010 <provic0099@gmail.com>
2021-10-08 11:37:11 +0800

    add logo and change project title

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -7,11 +7,16 @@ import Create from '../../pages/Create'
 import Wallet from '../Wallet'
 import styles from './Navbar.module.css'
 import NavbarItem from './NavbarItem'
+import logo from '../../assets/spaceLogo.png'
 function Navbar() {
     return (
         <Router>
             <div>
+                
                 <nav className={styles.spaceNav}>
+                    <a href="">
+                        <img src={logo} alt="mmtuts logo"/>
+                    </a>
                     <ul className={styles.spaceUnorderList}>
                         <NavbarItem navName = {"MarketPlace"} navPath = {"/"}/>
                         <NavbarItem navName = {"Create NFTs"} navPath = {"/create"}/>

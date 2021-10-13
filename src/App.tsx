cb40906c39cfb0b54cc3b4f0046de5345a8c7ee6
dragonyu1010 <provic0099@gmail.com>
2021-10-13 22:34:23 +0800

    add subside

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Wallet from './components/Wallet'
 import { Web3ReactProvider } from '@web3-react/core'
 import Web3 from 'web3'
 import styles from './App.module.css'
+import Sidebar from './components/Sidebar';
 
 function getLibrary(provider:any ) {
   return new Web3(provider)
@@ -13,7 +14,9 @@ function getLibrary(provider:any ) {
 function App() {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
+      <Sidebar />
       <Navbar/>
+      
     </Web3ReactProvider>
   );
 }

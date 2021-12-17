d8a1c744bbcd66ef6a02aa60fb8a77a9ebb712f4
dragonyu1010 <provic0099@gmail.com>
2021-12-17 23:07:22 +0800

    add owl-carousel-js for live-auction part and explorer part on the landing page

--- src/pages/Home/explorer/Explorer.tsx
+++ src/pages/Home/explorer/Explorer.tsx
@@ -1,3 +1,20 @@
+import itemImg1 from '../../../assets/img/items/1.jpg'
+import avatarImg1 from '../../../assets/img/avatars/1.jpg'
+import itemImg2 from '../../../assets/img/items/2.jpg'
+import avatarImg2 from '../../../assets/img/avatars/2.jpg'
+import itemImg3 from '../../../assets/img/items/3.jpg'
+import avatarImg3 from '../../../assets/img/avatars/3.jpg'
+import itemImg4 from '../../../assets/img/items/4.jpg'
+import avatarImg4 from '../../../assets/img/avatars/4.jpg'
+import itemImg5 from '../../../assets/img/items/5.jpg'
+import avatarImg5 from '../../../assets/img/avatars/5.jpg'
+import itemImg6 from '../../../assets/img/items/6.jpg'
+import avatarImg6 from '../../../assets/img/avatars/6.jpg'
+import itemImg7 from '../../../assets/img/items/7.jpg'
+import avatarImg7 from '../../../assets/img/avatars/7.jpg'
+import itemImg8 from '../../../assets/img/items/8.jpg'
+import avatarImg8 from '../../../assets/img/avatars/8.jpg'
+import * as FaIcons from 'react-icons/fa'
 function Explorer() {
     return (
 		<section className = "explore-area">
@@ -17,12 +34,12 @@ function Explorer() {
 							<div className = "main__carousel main__carousel--explore owl-carousel" id="explore">
 								<div className = "card">
 									<a href="item.html" className = "card__cover">
-										<img src="assets/img/items/8.jpg" alt="" />
+										<img src={itemImg8} alt="" />
 									</a>
 									<h3 className = "card__title"><a href="item.html">Awesome Color
 											Drop</a></h3>
 									<div className = "card__author card__author--verified">
-										<img src="assets/img/avatars/8.jpg" alt=""/>
+										<img src={avatarImg8} alt=""/>
 										<a href="author.html">@morgan28</a>
 									</div>
 									<div className = "card__info">
@@ -40,11 +57,11 @@ function Explorer() {
 
 								<div className = "card">
 									<a href="item.html" className = "card__cover">
-										<img src="assets/img/items/7.jpg" alt=""/>
+										<img src={itemImg7} alt=""/>
 									</a>
 									<h3 className = "card__title"><a href="item.html">Beautiful Leaf In The Wall</a></h3>
 									<div className = "card__author card__author--verified">
-										<img src="assets/img/avatars/7.jpg" alt=""/>
+										<img src={avatarImg7} alt=""/>
 										<a href="author.html">@nessler520</a>
 									</div>
 									<div className = "card__info">
@@ -62,11 +79,11 @@ function Explorer() {
 
 								<div className = "card">
 									<a href="item.html" className = "card__cover">
-										<img src="assets/img/items/6.jpg" alt=""/>
+										<img src={itemImg6} alt=""/>
 									</a>
 									<h3 className = "card__title"><a href="item.html">Blue Planet Revolution</a></h3>
 									<div className = "card__author card__author--verified">
-										<img src="assets/img/avatars/6.jpg" alt=""/>
+										<img src={avatarImg6} alt=""/>
 										<a href="author.html">@kimberly28</a>
 									</div>
 									<div className = "card__info">
@@ -84,12 +101,12 @@ function Explorer() {
 
 								<div className = "card">
 									<a href="item.html" className = "card__cover">
-										<img src="assets/img/items/5.jpg" alt=""/>
+										<img src={itemImg5} alt=""/>
 									</a>
 									<h3 className = "card__title"><a href="item.html">Colorful Fantacy
 											Flowers</a></h3>
 									<div className = "card__author card__author--verified">
-										<img src="assets/img/avatars/5.jpg" alt=""/>
+										<img src={avatarImg5} alt=""/>
 										<a href="author.html">@patricia458</a>
 									</div>
 									<div className = "card__info">
@@ -107,11 +124,11 @@ function Explorer() {
 
 								<div className = "card">
 									<a href="item.html" className = "card__cover">
-										<img src="assets/img/items/4.jpg" alt=""/>
+										<img src={itemImg4} alt=""/>
 									</a>
 									<h3 className = "card__title"><a href="item.html">Let's Go The Navy Blue Galaxies</a></h3>
 									<div className = "card__author card__author--verified">
-										<img src="assets/img/avatars/4.jpg" alt=""/>
+										<img src={avatarImg4} alt=""/>
 										<a href="author.html">@humphrey124</a>
 									</div>
 									<div className = "card__info">
@@ -130,12 +147,12 @@ function Explorer() {
 								<div className = "card">
 									<a href="https://www.youtube.com/watch?v=ckHzmP1evNU"
 										className = "card__cover card__cover--video open-video">
-										<img src="assets/img/items/3.jpg" alt=""/>
+										<img src={itemImg3} alt=""/>
 										<i className = "far fa-video"></i>
 									</a>
 									<h3 className = "card__title"><a href="item.html">3D Ethereum In Diamond</a></h3>
 									<div className = "card__author card__author--verified">
-										<img src="assets/img/avatars/3.jpg" alt=""/>
+										<img src={avatarImg3} alt=""/>
 										<a href="author.html">@sheilagne22</a>
 									</div>
 									<div className = "card__info">
@@ -153,11 +170,11 @@ function Explorer() {
 
 								<div className = "card">
 									<a href="item.html" className = "card__cover">
-										<img src="assets/img/items/2.jpg" alt=""/>
+										<img src={itemImg2} alt=""/>
 									</a>
 									<h3 className = "card__title"><a href="item.html">Fantacy Of Water Drop</a></h3>
 									<div className = "card__author card__author--verified">
-										<img src="assets/img/avatars/2.jpg" alt=""/>
+										<img src={avatarImg2} alt=""/>
 										<a href="author.html">@kimberly120</a>
 									</div>
 									<div className = "card__info">
@@ -175,10 +192,12 @@ function Explorer() {
 
 							</div>
 
-							<button className = "main__nav main__nav--prev" data-nav="#explore" type="button"><i
-									className = "far fa-arrow-left"></i></button>
-							<button className = "main__nav main__nav--next" data-nav="#explore" type="button"><i
-									className = "far fa-arrow-right"></i></button>
+							<button className = "main__nav main__nav--prev" data-nav="#explore" type="button">
+								<FaIcons.FaArrowLeft />
+							</button>
+							<button className = "main__nav main__nav--next" data-nav="#explore" type="button">
+								<FaIcons.FaArrowRight />
+							</button>
 						</div>
 					</div>
 					{/* end carousel */}
--- src/pages/Home/liveAuction/LiveAuction.tsx
+++ src/pages/Home/liveAuction/LiveAuction.tsx
@@ -1,4 +1,19 @@
-import img1 from '../../../assets/img/items/1.jpg'
+import itemImg1 from '../../../assets/img/items/1.jpg'
+import avatarImg1 from '../../../assets/img/avatars/1.jpg'
+import itemImg2 from '../../../assets/img/items/2.jpg'
+import avatarImg2 from '../../../assets/img/avatars/2.jpg'
+import itemImg3 from '../../../assets/img/items/3.jpg'
+import avatarImg3 from '../../../assets/img/avatars/3.jpg'
+import itemImg4 from '../../../assets/img/items/4.jpg'
+import avatarImg4 from '../../../assets/img/avatars/4.jpg'
+import itemImg5 from '../../../assets/img/items/5.jpg'
+import avatarImg5 from '../../../assets/img/avatars/5.jpg'
+import itemImg6 from '../../../assets/img/items/6.jpg'
+import avatarImg6 from '../../../assets/img/avatars/6.jpg'
+import itemImg7 from '../../../assets/img/items/7.jpg'
+import avatarImg7 from '../../../assets/img/avatars/7.jpg'
+
+import * as FaIcons from 'react-icons/fa'
 function LiveAuction() {
     return (
         <section className = "live-auction">
@@ -18,12 +33,12 @@ function LiveAuction() {
                             <div className = "main__carousel main__carousel--live owl-carousel" id="live">
                                 <div className = "card">
                                     <a href="item.html" className = "card__cover">
-                                        <img src={img1} alt=""/>
+                                        <img src={itemImg1} alt=""/>
                                         <span className = "card__time">40 minutes left</span>
                                     </a>
                                     <h3 className = "card__title"><a href="item.html">Fantacy Of Water Drop</a></h3>
                                     <div className = "card__author card__author--verified">
-                                        <img src="assets/img/avatars/1.jpg" alt=""/>
+                                        <img src={avatarImg1} alt=""/>
                                         <a href="author.html">@kimberly120</a>
                                     </div>
                                     <div className = "card__info">
@@ -42,13 +57,13 @@ function LiveAuction() {
                                 <div className = "card">
                                     <a href="https://www.youtube.com/watch?v=ckHzmP1evNU"
                                         className = "card__cover card__cover--video open-video">
-                                        <img src="assets/img/items/2.jpg" alt=""/>
+                                        <img src={itemImg2} alt=""/>
                                         <i className = "far fa-video"></i>
                                         <span className = "card__time">1 hour left</span>
                                     </a>
                                     <h3 className = "card__title"><a href="item.html">3D Ethereum In Diamond</a></h3>
                                     <div className = "card__author card__author--verified">
-                                        <img src="assets/img/avatars/2.jpg" alt=""/>
+                                        <img src={avatarImg2} alt=""/>
                                         <a href="author.html">@sheilagne22</a>
                                     </div>
                                     <div className = "card__info">
@@ -66,7 +81,7 @@ function LiveAuction() {
 
                                 <div className = "card">
                                     <a href="item.html" className = "card__cover">
-                                        <img src="assets/img/items/3.jpg" alt=""/>
+                                        <img src={itemImg3} alt=""/>
                                         <span className = "card__time card__time--clock">
                                             <i className = "far fa-fire"></i>
                                             <span className = "card__clock card__clock--1"></span>
@@ -74,7 +89,7 @@ function LiveAuction() {
                                     </a>
                                     <h3 className = "card__title"><a href="item.html">Let's Go The Navy Blue Galaxies</a></h3>
                                     <div className = "card__author card__author--verified">
-                                        <img src="assets/img/avatars/3.jpg" alt=""/>
+                                        <img src={avatarImg3} alt=""/>
                                         <a href="author.html">@humphrey124</a>
                                     </div>
                                     <div className = "card__info">
@@ -92,13 +107,13 @@ function LiveAuction() {
 
                                 <div className = "card">
                                     <a href="#modal-asset" className = "card__cover open-modal">
-                                        <img src="assets/img/items/4.jpg" alt=""/>
+                                        <img src={itemImg4} alt=""/>
                                         <span className = "card__time">Popup</span>
                                     </a>
                                     <h3 className = "card__title"><a href="#modal-asset" className = "open-modal">Colorful Fantacy
                                             Flowers</a></h3>
                                     <div className = "card__author card__author--verified">
-                                        <img src="assets/img/avatars/4.jpg" alt=""/>
+                                        <img src={avatarImg4} alt=""/>
                                         <a href="author.html">@patricia458</a>
                                     </div>
                                     <div className = "card__info">
@@ -116,7 +131,7 @@ function LiveAuction() {
 
                                 <div className = "card">
                                     <a href="item.html" className = "card__cover">
-                                        <img src="assets/img/items/5.jpg" alt=""/>
+                                        <img src={itemImg5} alt=""/>
                                         <span className = "card__time card__time--clock">
                                             <i className = "far fa-fire"></i>
                                             <span className = "card__clock card__clock--2"></span>
@@ -124,7 +139,7 @@ function LiveAuction() {
                                     </a>
                                     <h3 className = "card__title"><a href="item.html">Blue Planet Revolution</a></h3>
                                     <div className = "card__author card__author--verified">
-                                        <img src="assets/img/avatars/5.jpg" alt=""/>
+                                        <img src={avatarImg5} alt=""/>
                                         <a href="author.html">@kimberly28</a>
                                     </div>
                                     <div className = "card__info">
@@ -142,12 +157,12 @@ function LiveAuction() {
 
                                 <div className = "card">
                                     <a href="item.html" className = "card__cover">
-                                        <img src="assets/img/items/6.jpg" alt=""/>
+                                        <img src={itemImg6} alt=""/>
                                         <span className = "card__time">25 minutes left</span>
                                     </a>
                                     <h3 className = "card__title"><a href="item.html">Beautiful Leaf In The Wall</a></h3>
                                     <div className = "card__author card__author--verified">
-                                        <img src="assets/img/avatars/6.jpg" alt=""/>
+                                        <img src={avatarImg6} alt=""/>
                                         <a href="author.html">@nessler520</a>
                                     </div>
                                     <div className = "card__info">
@@ -165,13 +180,13 @@ function LiveAuction() {
 
                                 <div className = "card">
                                     <a href="#modal-asset" className = "card__cover open-modal">
-                                        <img src="assets/img/items/7.jpg" alt=""/>
+                                        <img src={itemImg7} alt=""/>
                                         <span className = "card__time">Popup</span>
                                     </a>
                                     <h3 className = "card__title"><a href="#modal-asset" className = "open-modal">Awesome Color
                                             Drop</a></h3>
                                     <div className = "card__author card__author--verified">
-                                        <img src="assets/img/avatars/7.jpg" alt=""/>
+                                        <img src={avatarImg7} alt=""/>
                                         <a href="author.html">@morgan28</a>
                                     </div>
                                     <div className = "card__info">
@@ -188,10 +203,14 @@ function LiveAuction() {
                                 </div>
                             </div>
 
-                            <button className = "main__nav main__nav--prev" data-nav="#live" type="button"><i
-                                    className = "far fa-arrow-left"></i></button>
-                            <button className = "main__nav main__nav--next" data-nav="#live" type="button"><i
-                                    className = "far fa-arrow-right"></i></button>
+                            <button className = "main__nav main__nav--prev" data-nav="#live" type="button">
+                                <i className = "far fa-arrow-left"></i>
+                                <FaIcons.FaArrowLeft />
+                            </button>
+                            <button className = "main__nav main__nav--next" data-nav="#live" type="button">
+                                <i className = "far fa-arrow-right"></i>
+                                <FaIcons.FaArrowRight />
+                            </button>
                         </div>
                     </div>
                     {/* end carousel */}

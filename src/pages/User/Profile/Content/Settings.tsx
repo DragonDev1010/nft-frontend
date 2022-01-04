30ace5b8897441f25a66c97efc17697f0c9f39ee
dragonyu1010 <provic0099@gmail.com>
2022-01-04 10:26:51 +0800

    connect user-profile-setting with backend

--- src/pages/User/Profile/Content/Settings.tsx
+++ src/pages/User/Profile/Content/Settings.tsx
@@ -1,4 +1,81 @@
+import {useState, useEffect} from 'react'
 function Settings() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [facebook, setFacebook] = useState('')
+    const [insta, setInsta] = useState('')
+    const [twitter, setTwitter] = useState('')
+    const [tele, setTele] = useState('')
+    function fetchData() {
+        let wallet = localStorage.getItem('wallet')
+        let fetchUrl = process.env.REACT_APP_API_BASE_URL + 'users/' + wallet
+        fetch(fetchUrl)
+            .then(res => res.json())
+            .then( res => {
+                if(res[0] !== undefined){
+                    if(res[0].name !== undefined)
+                        setName(res[0].name)
+                    if(res[0].email !== undefined)
+                        setEmail(res[0].email)
+                    if(res[0].firstName !== undefined)
+                        setFirstName(res[0].firstName)
+                    if(res[0].lastName !== undefined)
+                        setLastName(res[0].lastName)
+                    if(res[0].facebook !== undefined)
+                        setFacebook(res[0].facebook)                    
+                    if(res[0].twitter !== undefined)
+                        setTwitter(res[0].twitter)
+                    if(res[0].instagram !== undefined)
+                        setInsta(res[0].instagram)
+                    if(res[0].tele !== undefined)
+                        setTele(res[0].tele)
+                }
+            })
+    }
+    function update(event:any) {
+        event.preventDefault()
+        var data = new FormData()
+        data.append('name', name)
+        data.append('email', email)
+        data.append('firstName', firstName)
+        data.append('lastName', lastName)
+        data.append('facebook', facebook)
+        data.append('instagram', insta)
+        data.append('twitter', twitter)
+        data.append('tele', tele)
+
+        let wallet = localStorage.getItem('wallet')
+        let fetchUrl = process.env.REACT_APP_API_BASE_URL + 'users/' + wallet
+        fetch(fetchUrl, {
+            method: 'PUT',
+            body: data
+        }).then(res => res.json())
+            .then(res => {
+                if(res[0] !== undefined){
+                    if(res[0].name !== undefined)
+                        setName(res[0].name)
+                    if(res[0].email !== undefined)
+                        setEmail(res[0].email)
+                    if(res[0].firstName !== undefined)
+                        setFirstName(res[0].firstName)
+                    if(res[0].lastName !== undefined)
+                        setLastName(res[0].lastName)
+                    if(res[0].facebook !== undefined)
+                        setFacebook(res[0].facebook)                    
+                    if(res[0].twitter !== undefined)
+                        setTwitter(res[0].twitter)
+                    if(res[0].instagram !== undefined)
+                        setInsta(res[0].instagram)
+                    if(res[0].tele !== undefined)
+                        setTele(res[0].tele)
+                }
+            })
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
     return(
         <div className="tab-pane fade" id="tab-4" role="tabpanel">
             <div className="row row--grid">
@@ -14,7 +91,7 @@ function Settings() {
                                 <div className="sign__group">
                                     <label className="sign__label" htmlFor="username">Username</label>
                                     <input id="username" type="text" name="username" className="sign__input"
-                                        placeholder="condrey21"/>
+                                        placeholder={name} onChange={(e:any) => {setName(e.target.value)}}/>
                                 </div>
                             </div>
 
@@ -22,7 +99,7 @@ function Settings() {
                                 <div className="sign__group">
                                     <label className="sign__label" htmlFor="email">Email</label>
                                     <input id="email" type="text" name="email" className="sign__input"
-                                        placeholder="test@example.com"/>
+                                        placeholder={email} onChange={(e:any) => {setEmail(e.target.value)}}/>
                                 </div>
                             </div>
 
@@ -30,7 +107,7 @@ function Settings() {
                                 <div className="sign__group">
                                     <label className="sign__label" htmlFor="firstname">First Name</label>
                                     <input id="firstname" type="text" name="firstname"
-                                        className="sign__input" placeholder="Susa"/>
+                                        className="sign__input" placeholder={firstName} onChange={(e:any) => {setFirstName(e.target.value)}}/>
                                 </div>
                             </div>
 
@@ -38,12 +115,12 @@ function Settings() {
                                 <div className="sign__group">
                                     <label className="sign__label" htmlFor="lastname">Last name</label>
                                     <input id="lastname" type="text" name="lastname" className="sign__input"
-                                        placeholder="Condrey"/>
+                                        placeholder={lastName} onChange={(e:any) => {setLastName(e.target.value)}}/>
                                 </div>
                             </div>
 
                             <div className="col-12">
-                                <button className="sign__btn" type="button">Save</button>
+                                <button className="sign__btn" type="button" onClick={update}>Save</button>
                             </div>
                         </div>
                     </form>
@@ -62,7 +139,7 @@ function Settings() {
                                 <div className="sign__group">
                                     <label className="sign__label" htmlFor="facebook">Facebook</label>
                                     <input id="facebook" type="text" name="facebook" className="sign__input"
-                                        placeholder="https://www.facebook.com/"/>
+                                        placeholder={facebook} onChange={(e:any) => {setFacebook(e.target.value)}}/>
                                 </div>
                             </div>
 
@@ -70,7 +147,7 @@ function Settings() {
                                 <div className="sign__group">
                                     <label className="sign__label" htmlFor="twitter">Twitter</label>
                                     <input id="twitter" type="text" name="twitter" className="sign__input"
-                                        placeholder="https://twitter.com/?lang=en"/>
+                                        placeholder={twitter} onChange={(e:any) => {setTwitter(e.target.value)}}/>
                                 </div>
                             </div>
 
@@ -78,7 +155,7 @@ function Settings() {
                                 <div className="sign__group">
                                     <label className="sign__label" htmlFor="firstname">Instagram</label>
                                     <input id="instagram" type="text" name="instagram"
-                                        className="sign__input" placeholder="https://www.instagram.com/"/>
+                                        className="sign__input" placeholder={insta} onChange={(e:any) => {setInsta(e.target.value)}} />
                                 </div>
                             </div>
 
@@ -86,12 +163,12 @@ function Settings() {
                                 <div className="sign__group">
                                     <label className="sign__label" htmlFor="telegram">Telegram</label>
                                     <input id="telegram" type="text" name="telegram" className="sign__input"
-                                        placeholder="t.me/"/>
+                                        placeholder={tele} onChange={(e:any) => {setTele(e.target.value)}}/>
                                 </div>
                             </div>
 
                             <div className="col-12">
-                                <button className="sign__btn" type="button">Update</button>
+                                <button className="sign__btn" type="button" onClick={update}>Update</button>
                             </div>
                         </div>
                     </form>

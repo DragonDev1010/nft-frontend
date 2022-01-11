6ea7c6e5e539cd3ac8690c0d091d9d45a1693057
dragonyu1010 <provic0099@gmail.com>
2022-01-11 17:18:51 +0800

    get NFT image from backend on NFT Detail page

--- src/pages/NftDetail/Detail/Content/Content.tsx
+++ src/pages/NftDetail/Detail/Content/Content.tsx
@@ -3,11 +3,35 @@ import FavCount from './FavCount'
 import SocialLink from './SocialLink'
 import {useState, useEffect} from 'react'
 function Content(props: any) {
+    const nftId = props.nftId
+    const [image, setImage] = useState('')
     const [favCount, setFavCount] = useState(0)
+    function arrayBufferToBase64(buffer:any) {
+        var binary = '';
+        var bytes = [].slice.call(new Uint8Array(buffer));
+        bytes.forEach((b) => binary += String.fromCharCode(b));
+        return window.btoa(binary);
+    }
+    function getImage(buffer:any) {
+        let temp = 'data:image/jpeg;base64,' + arrayBufferToBase64(buffer)
+        setImage(temp)
+    }
+    function fetchData() {
+        let fetchURL = process.env.REACT_APP_API_BASE_URL + 'nfts/' + nftId
+        fetch(fetchURL)
+            .then(res=>res.json())
+            .then(res => {
+                if(res[0].img.data.data !== undefined) 
+                    getImage(res[0].img.data.data)
+            })
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
     return(
         <div className="col-12 col-xl-8">
             <div className="asset__item">
-                <img src={single} alt=""/>
+                <img src={image} alt=""/>
                 <SocialLink/>
                 <FavCount favCnt = {favCount}/>
             </div>

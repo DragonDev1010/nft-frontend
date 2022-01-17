ae5b153ef8b05b95be6514b2fa362275918be256
dragonyu1010 <provic0099@gmail.com>
2022-01-17 11:04:44 +0800

    connect topCreator component with backend

--- src/pages/Home/topCreators/TopCreators.tsx
+++ src/pages/Home/topCreators/TopCreators.tsx
@@ -1,19 +1,16 @@
-import img1 from '../../../assets/img/avatars/1.jpg'
-import img2 from '../../../assets/img/avatars/2.jpg'
-import img3 from '../../../assets/img/avatars/3.jpg'
-import img4 from '../../../assets/img/avatars/3.jpg'
-import img5 from '../../../assets/img/avatars/3.jpg'
-import img6 from '../../../assets/img/avatars/3.jpg'
-import img7 from '../../../assets/img/avatars/3.jpg'
-import img8 from '../../../assets/img/avatars/3.jpg'
-import img9 from '../../../assets/img/avatars/3.jpg'
-import img10 from '../../../assets/img/avatars/3.jpg'
-import img11 from '../../../assets/img/avatars/3.jpg'
-import img12 from '../../../assets/img/avatars/3.jpg'
-import img13 from '../../../assets/img/avatars/3.jpg'
-import img14 from '../../../assets/img/avatars/3.jpg'
-import img15 from '../../../assets/img/avatars/3.jpg'
+import TopCreatorItem from './TopCreatorItem'
+import {useState, useEffect} from 'react'
+
 function TopCreator() {
+    const [creators, setCreators] = useState<any>([])
+    useEffect(() => {
+        let fetchURL = process.env.REACT_APP_API_BASE_URL + 'top_creators' 
+        fetch(fetchURL)
+            .then(res => res.json())
+            .then(res=>{
+                setCreators([...res])
+            })
+    }, [])
     return (
         <section className = "top-seller">
             <div className = "container">
@@ -22,149 +19,25 @@ function TopCreator() {
                     <div className = "col-12">
                         <div className = "main__title">
                             <h2>Top Creators</h2>
-                            <a href="authors.html" className = "main__link">View All <i className = "far fa-arrow-right"></i></a>
+                            {/* <a href="" className = "main__link">View All <i className = "far fa-arrow-right"></i></a> */}
                         </div>
                     </div>
                     {/* end title */}
 
                     {/* sellers list */}
                     <div className = "col-12">
-                        <ul className = "sellers-list">
-                            <li>
-                                <span className = "sellers-list__number">1</span>
-                                <div className = "sellers-list__author sellers-list__author--verified">
-                                    <img src={img1} alt=""/>
-                                    <a href="author.html">@kimberly120</a>
-                                    <span>520.25 ETH</span>
-                                </div>
-                            </li>
-
-                            <li>
-                                <span className = "sellers-list__number">2</span>
-                                <div className = "sellers-list__author sellers-list__author--verified">
-                                    <img src={img2} alt=""/>
-                                    <a href="author.html">@sheilagne22</a>
-                                    <span>310.18 ETH</span>
-                                </div>
-                            </li>
-
-                            <li>
-                                <span className = "sellers-list__number">3</span>
-                                <div className = "sellers-list__author sellers-list__author--verified">
-                                    <img src={img3} alt=""/>
-                                    <a href="author.html">@humphrey124</a>
-                                    <span>288.65 ETH</span>
-                                </div>
-                            </li>
-
-                            <li>
-                                <span className = "sellers-list__number">4</span>
-                                <div className = "sellers-list__author sellers-list__author--verified">
-                                    <img src={img4} alt=""/>
-                                    <a href="author.html">@patricia458</a>
-                                    <span>150.98 ETH</span>
-                                </div>
-                            </li>
-
-                            <li>
-                                <span className = "sellers-list__number">5</span>
-                                <div className = "sellers-list__author sellers-list__author--verified">
-                                    <img src={img5} alt=""/>
-                                    <a href="author.html">@kimberly28</a>
-                                    <span>132.3 ETH</span>
-                                </div>
-                            </li>
-
-                            <li>
-                                <span className = "sellers-list__number">6</span>
-                                <div className = "sellers-list__author sellers-list__author--verified">
-                                    <img src={img6} alt=""/>
-                                    <a href="author.html">@nessler520</a>
-                                    <span>95.72 ETH</span>
-                                </div>
-                            </li>
-
-                            <li>
-                                <span className = "sellers-list__number">7</span>
-                                <div className = "sellers-list__author sellers-list__author--verified">
-                                    <img src={img7} alt=""/>
-                                    <a href="author.html">@morgan28</a>
-                                    <span>84.56 ETH</span>
-                                </div>
-                            </li>
-
-                            <li>
-                                <span className = "sellers-list__number">8</span>
-                                <div className = "sellers-list__author sellers-list__author--verified">
-                                    <img src={img8} alt=""/>
-                                    <a href="author.html">@michalak952</a>
-                                    <span>75.15 ETH</span>
-                                </div>
-                            </li>
-
-                            <li>
-                                <span className = "sellers-list__number">9</span>
-                                <div className = "sellers-list__author sellers-list__author--verified">
-                                    <img src={img9} alt=""/>
-                                    <a href="author.html">@wheeler77</a>
-                                    <span>51.00 ETH</span>
-                                </div>
-                            </li>
-
-                            <li>
-                                <span className = "sellers-list__number">10</span>
-                                <div className = "sellers-list__author sellers-list__author--verified">
-                                    <img src={img10} alt=""/>
-                                    <a href="author.html">@donna211</a>
-                                    <span>49.30 ETH</span>
-                                </div>
-                            </li>
-
-                            <li>
-                                <span className = "sellers-list__number">11</span>
-                                <div className = "sellers-list__author sellers-list__author--verified">
-                                    <img src={img11} alt=""/>
-                                    <a href="author.html">@samuels26</a>
-                                    <span>42.66 ETH</span>
-                                </div>
-                            </li>
-
-                            <li>
-                                <span className = "sellers-list__number">12</span>
-                                <div className = "sellers-list__author sellers-list__author--verified">
-                                    <img src={img12} alt=""/>
-                                    <a href="author.html">@freda100</a>
-                                    <span>31.79 ETH</span>
-                                </div>
-                            </li>
-
-                            <li>
-                                <span className = "sellers-list__number">13</span>
-                                <div className = "sellers-list__author sellers-list__author--verified">
-                                    <img src={img13} alt=""/>
-                                    <a href="author.html">@gipson005</a>
-                                    <span>30.25 ETH</span>
-                                </div>
-                            </li>
-
-                            <li>
-                                <span className = "sellers-list__number">14</span>
-                                <div className = "sellers-list__author sellers-list__author--verified">
-                                    <img src={img14} alt=""/>
-                                    <a href="author.html">@helen22 </a>
-                                    <span>28.33 ETH</span>
-                                </div>
-                            </li>
-
-                            <li>
-                                <span className = "sellers-list__number">15</span>
-                                <div className = "sellers-list__author sellers-list__author--verified">
-                                    <img src={img15} alt=""/>
-                                    <a href="author.html">@wilson202</a>
-                                    <span>25.95 ETH</span>
-                                </div>
-                            </li>
-                        </ul>
+                        {
+                            creators.length === 0 ?
+                                ""
+                            :
+                                <ul className = "sellers-list">
+                                {
+                                    creators.map((creator:any, idx:any) => (
+                                        <TopCreatorItem order={idx} name ={creator.name} avatar={creator.userImg}/>                            
+                                    ))
+                                }
+                                </ul>
+                        }
                     </div>
                     {/* end sellers list */}
                 </div>

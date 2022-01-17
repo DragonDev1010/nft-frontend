a75ea5b6bf768891d6c56753feb1ffdf44692c1e
dragonyu1010 <provic0099@gmail.com>
2022-01-18 03:41:34 +0800

    fix infinite loop in nft marketplace page

--- src/pages/Marketplace/explorerArea/content/Content.tsx
+++ src/pages/Marketplace/explorerArea/content/Content.tsx
@@ -1,64 +1,14 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef} from 'react'
 import { useSelector } from 'react-redux'
 import Card from './Card'
 import ReactPaginate from 'react-paginate';
-function Content() {
-    let filterState = useSelector((state: any) => state.search)
-    const [allNfts, setAllNfts] = useState([])
-    const [nftsPerPage, setNftsPerPage] = useState([])
-    const [pages, setPages] = useState(1)
-    const [curPage, setCurPage] = useState(1)
-
-    function fetchNfts() {
-        let querySearch = filterState.search
-        let querySort = filterState.sort
-        let queryCategory = filterState.category
-        let queryPrice = filterState.price
-        let query = ''
-        if(querySearch !== undefined && querySearch !== '' && querySearch !== null) {
-            query += 'search[search]=' + querySearch + '&'
-        }
-        if(querySort !== undefined && querySort !== null) {
-            query += 'search[sort]=' + querySort + '&'
-        }
-        if(queryCategory.length > 0) {
-            queryCategory.map((item:any) => {
-                query += 'search[category]=' + item + '&'
-            })
-        }
-        if(queryPrice.min > 0 || queryPrice.max < Number.MAX_SAFE_INTEGER) {
-            query += 'search[price][min]=' + queryPrice.min + '&'
-            query += 'search[price][max]=' + queryPrice.max + '&'
-        }
-        let fetchURL = process.env.REACT_APP_API_BASE_URL + 'nfts?' + query
-        console.log(fetchURL)
-        fetch(fetchURL)
-            .then(res => res.json())
-            .then( res => {
-                setAllNfts(res)
-                setPages(Math.floor(res.length/3) + 1)
-            })
-    }
-    function handlePageClick(e:any) {
-        setCurPage(e.selected)
-    }
-    useEffect(() => {
-        console.log(filterState)
-        if(allNfts.length === 0) {
-            fetchNfts()
-        }
-        let temp = allNfts.slice(curPage*3, (curPage+1)*3)
-        setNftsPerPage([...temp])
-        console.log(nftsPerPage)
-    // }, [filterState])
-    }, [curPage, nftsPerPage, filterState])
+function Items({currentItems}:any) {
     return(
-        <div className="col-12 col-xl-9">
-            <div className="row row--grid">
-                {
-                    nftsPerPage.length > 0 ?
-                    nftsPerPage.map((item:any, idx:any) => (
-                        <Card 
+        <div className="row row--grid">
+            {
+                currentItems &&
+                currentItems.map((item:any) => (
+                    <Card 
                             nftId={item.nft_id}
                             nftImg={item.img}
                             nftName={item.name} 
@@ -67,18 +17,53 @@ function Content() {
                             price={item.price}
                             state = {item.state}
                             favUsers = {item.favUsers}
-                        />
-                    ))
-                    :
-                    ""
-                }
-            </div>
-
+                    />
+                ))
+            }
+        </div>
+    )
+}
+function Content() {
+    const itemsPerPage = 3
+    const [currentItems, setCurrentItems] = useState(null);
+    const [pageCount, setPageCount] = useState(0);
+    const [itemOffset, setItemOffset] = useState(0);
+    const [allNfts, setAllNfts] = useState<any>([])
+    const handlePageClick = (event:any) => {
+        const newOffset = (event.selected * itemsPerPage) % allNfts.length;
+        console.log(
+          `User requested page number ${event.selected}, which is offset ${newOffset}`
+        );
+        setItemOffset(newOffset);
+    };
+    function getAllNfts() {
+        let fetchURL = process.env.REACT_APP_API_BASE_URL + 'nfts'
+        fetch(fetchURL)
+            .then(res => res.json())
+            .then(res=>{
+                setAllNfts([...res])
+            })
+    }
+    useEffect(() => {
+        getAllNfts()
+    }, [])
+    useEffect(() => {
+        if(allNfts.length > 0) {
+            const endOffset = itemOffset + itemsPerPage;
+            console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+            setCurrentItems(allNfts.slice(itemOffset, endOffset));
+            setPageCount(Math.ceil(allNfts.length / itemsPerPage));
+        }
+    }, [itemOffset, itemsPerPage, allNfts])
+    return(
+        <div className="col-12 col-xl-9">
+            <Items currentItems={currentItems} />
             <ReactPaginate
                 previousLabel={'\u2190'}
                 nextLabel={'\u2192'}
-                pageCount={pages}
+                pageCount={pageCount}
                 onPageChange={handlePageClick}
+
                 containerClassName={'pagination'}
                 activeClassName={'active'}
                 className='paginator__list'
